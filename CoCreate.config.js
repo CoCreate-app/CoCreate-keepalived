ed67ef005eee68c666e37693444adefb7d3b2b7d
Frank Pagan <frank@cocreate.app>
2022-02-09 21:46:06 +0000

    fix: add document_id to CoCreate.config

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,7 +8,7 @@ module.exports = {
         {
             "entry": "./docs/index.html",
             "collection": "files",
-            "document_id": "",
+            "document_id": "6204253f80b409001727b742",
             "key": "src",
             "data": {
                 "name": "index.html",
